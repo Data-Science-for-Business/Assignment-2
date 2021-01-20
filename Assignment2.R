64603c3ca9dc90c1c7675b6b1de21a9bfba2ba19
KevinDECROCK <77623420+KevinDECROCK@users.noreply.github.com>
2021-01-20 21:27:42 +0100

    Update Assignment2.R

--- Assignment2.R
+++ Assignment2.R
@@ -1,181 +1,255 @@
-#Assignment 2 DSB P3
-#Author: Nathan Hart
-#Date: 1/19/21
+#------------------------------------------------------------------------------#
+#--------------------------IMPORT LIBRARIES------------------------------------#
+#------------------------------------------------------------------------------#
+
+if("pacman" %in% rownames(installed.packages()) == FALSE) {install.packages("pacman")}
+
+pacman::p_load("caret","ROCR","lift","glmnet","MASS","e1071", "tidyverse", "dplyr", "GGally") #Check, and if needed install the necessary packages
+
+cd_df<-read.csv(file.choose(), na.strings=c(""," ","NA"), header=TRUE, stringsAsFactors = TRUE, sep = ";") # Load "CSV_DSB_S8_9_Credit"
 
 
-if("pacman" %in% rownames(installed.packages()) == FALSE) {install.packages("pacman")} # Check if you have universal installer package, install if not
 
-pacman::p_load("caret","ROCR","lift","glmnet","MASS","e1071") #Check, and if needed install the necessary packages
-
-Credit_Data<-read.csv(file.choose(), na.strings=c(""," ","NA"), header=TRUE, stringsAsFactors = TRUE) # Load the datafile to R
-New_Apps_Data<-read.csv(file.choose(), na.strings=c(""," ","NA"), header=TRUE, stringsAsFactors = TRUE) # Load the datafile to R
-
-str(Credit_Data) # See if some data types were misclassified when importing data from CSV
-str(New_Apps_Data)
-
-#-----------------------------------
-#fix data types here
-#-----------------------------------
-Credit_Data$ID <- as.factor(Credit_Data$ID) #maybe this should stay as numeric - represents chronological order? Assumption. Leaving as is for now
-Credit_Data$SEX <- as.factor(Credit_Data$SEX)
-Credit_Data$EDUCATION <- as.factor(Credit_Data$EDUCATION)
-Credit_Data$MARRIAGE <- as.factor(Credit_Data$MARRIAGE)
-Credit_Data$PAY_1 <- as.factor(Credit_Data$PAY_1)
-Credit_Data$PAY_2 <- as.factor(Credit_Data$PAY_2)
-Credit_Data$PAY_3 <- as.factor(Credit_Data$PAY_3)
-Credit_Data$PAY_4 <- as.factor(Credit_Data$PAY_4)
-Credit_Data$PAY_5 <- as.factor(Credit_Data$PAY_5)
-Credit_Data$PAY_6 <- as.factor(Credit_Data$PAY_6)
-Credit_Data$default_0 <- as.factor(Credit_Data$default_0)
-
-New_Apps_Data$SEX <- as.factor(New_Apps_Data$SEX)
-New_Apps_Data$EDUCATION <- as.factor(New_Apps_Data$EDUCATION)
-New_Apps_Data$MARRIAGE <- as.factor(New_Apps_Data$MARRIAGE)
-New_Apps_Data$PAY_1 <- as.factor(New_Apps_Data$PAY_1)
-New_Apps_Data$PAY_2 <- as.factor(New_Apps_Data$PAY_2)
-New_Apps_Data$PAY_3 <- as.factor(New_Apps_Data$PAY_3)
-New_Apps_Data$PAY_4 <- as.factor(New_Apps_Data$PAY_4)
-New_Apps_Data$PAY_5 <- as.factor(New_Apps_Data$PAY_5)
-New_Apps_Data$PAY_6 <- as.factor(New_Apps_Data$PAY_6)
-New_Apps_Data$default_0 <- as.factor(New_Apps_Data$default_0)
+str(cd_df) # See if some data types were misclassified when importing data from CSV
+View(cd_df) #cd_df stands for "credit data dataframe"
 
 
-str(Credit_Data) # Check the classifications make sense now
-str(New_Apps_Data)
+
+#------------------------------------------------------------------------------#
+#---------------------------CLEAN THE DATA-------------------------------------#
+#------------------------------------------------------------------------------#
+
+cd_df$ID <- as.factor(cd_df$ID)
+cd_df$SEX <- as.factor(cd_df$SEX)
+cd_df$EDUCATION <- as.factor(cd_df$EDUCATION)
+cd_df$MARRIAGE <- as.factor(cd_df$MARRIAGE)
+
+cd_df$PAY_1 <- as.factor(cd_df$PAY_1)
+cd_df$PAY_2 <- as.factor(cd_df$PAY_2)
+cd_df$PAY_3 <- as.factor(cd_df$PAY_3)
+cd_df$PAY_4 <- as.factor(cd_df$PAY_4)
+cd_df$PAY_5 <- as.factor(cd_df$PAY_5)
+cd_df$PAY_6 <- as.factor(cd_df$PAY_6)
+
+#cd_df$BILL_AMT1 <- as.numeric(levels(cd_df$BILL_AMT1))
+#cd_df$BILL_AMT2 <- as.integer(as.character(cd_df$BILL_AMT2))
+#cd_df$BILL_AMT3 <- as.numeric(as.character(cd_df$BILL_AMT3))
+#cd_df$BILL_AMT4 <- as.integer(as.character(cd_df$BILL_AMT4))
+#cd_df$BILL_AMT5 <- as.integer(as.character(cd_df$BILL_AMT5))
+#cd_df$BILL_AMT6 <- as.integer(as.character(cd_df$BILL_AMT6))
+#cd_df$PAY_AMT1 <- as.numeric(as.character(cd_df$PAY_AMT1))
+#cd_df$PAY_AMT2 <- as.numeric(as.character(cd_df$PAY_AMT2))
+#cd_df$PAY_AMT3 <- as.numeric(as.character(cd_df$PAY_AMT3))
+#cd_df$PAY_AMT4 <- as.numeric(as.character(cd_df$PAY_AMT4))
+#cd_df$PAY_AMT5 <- as.numeric(as.character(cd_df$PAY_AMT5))
+#cd_df$PAY_AMT6 <- as.numeric(as.character(cd_df$PAY_AMT6))
+
+cd_df$default_0 <- as.factor(cd_df$default_0)
+
+#for some reason the above line create a new column in some of our computers (but not to all)
+#Delete the wrongly created line
+#colnames(cd_df)
+#cd_df = subset(cd_df, select = -c(default_0...))
+
+cd_df = subset(cd_df, select = -c(X))
+
+str(cd_df)
+View(cd_df)
 
 
-# Create a custom function to fix missing values ("NAs") and preserve the NA info as surrogate variables
-fixNAs<-function(data_frame){
-  # Define reactions to NAs
-  integer_reac<-0
-  factor_reac<-"FIXED_NA"
-  character_reac<-"FIXED_NA"
-  date_reac<-as.Date("1900-01-01")
-  # Loop through columns in the data frame and depending on which class the variable is, apply the defined reaction and create a surrogate
-  
-  for (i in 1 : ncol(data_frame)){
-    if (class(data_frame[,i]) %in% c("numeric","integer")) {
-      if (any(is.na(data_frame[,i]))){
-        data_frame[,paste0(colnames(data_frame)[i],"_surrogate")]<-
-          as.factor(ifelse(is.na(data_frame[,i]),"1","0"))
-        data_frame[is.na(data_frame[,i]),i]<-integer_reac
-      }
-    } else
-      if (class(data_frame[,i]) %in% c("factor")) {
-        if (any(is.na(data_frame[,i]))){
-          data_frame[,i]<-as.character(data_frame[,i])
-          data_frame[,paste0(colnames(data_frame)[i],"_surrogate")]<-
-            as.factor(ifelse(is.na(data_frame[,i]),"1","0"))
-          data_frame[is.na(data_frame[,i]),i]<-factor_reac
-          data_frame[,i]<-as.factor(data_frame[,i])
-          
-        } 
-      } else {
-        if (class(data_frame[,i]) %in% c("character")) {
-          if (any(is.na(data_frame[,i]))){
-            data_frame[,paste0(colnames(data_frame)[i],"_surrogate")]<-
-              as.factor(ifelse(is.na(data_frame[,i]),"1","0"))
-            data_frame[is.na(data_frame[,i]),i]<-character_reac
-          }  
-        } else {
-          if (class(data_frame[,i]) %in% c("Date")) {
-            if (any(is.na(data_frame[,i]))){
-              data_frame[,paste0(colnames(data_frame)[i],"_surrogate")]<-
-                as.factor(ifelse(is.na(data_frame[,i]),"1","0"))
-              data_frame[is.na(data_frame[,i]),i]<-date_reac
-            }
-          }  
-        }       
-      }
-  } 
-  return(data_frame) 
-}
+#------------------------------------------------------------------------------#
+#---------------------------CHECK FOR MISSING VALUES---------------------------#
+#------------------------------------------------------------------------------#
 
-Credit_Data<-fixNAs(Credit_Data) #Apply fixNAs function to the data to fix missing values
-New_Apps_Data<-fixNAs(New_Apps_Data) #Apply fixNAs function to the data to fix missing values
+colSums(is.na(cd_df))
 
+#------------------------------------------------------------------------------#
+#---------------------------CORRECT DATA---------------------------------------#
+#------------------------------------------------------------------------------#
 
-#----------------------------------------------------------------------------------
-#-----------------------As far as I got tonight -----------------------------------
-#----------------------------------------------------------------------------------
+cd_df$MARRIAGE[cd_df$MARRIAGE == 0] <- 3
+cd_df$EDUCATION[cd_df$EDUCATION == 0] <- 5
+cd_df$EDUCATION[cd_df$EDUCATION == 6] <- 5
 
-table(Credit_Data$Group.State)# check for rare categories
+#create a dummy variable for uncommon account "negative billing" at AMT 1
+cd_df <- cd_df %>% add_column(NEG_BILL=0,.before = "BILL_AMT1")
+cd_df$NEG_BILL[cd_df$BILL_AMT1 <0] <- 1
 
+View(cd_df)
 
-# Create another a custom function to combine rare categories into "Other."+the name of the original variable (e.g., Other.State)
-# This function has two arguments: the name of the dataframe and the count of observation in a category to define "rare"
-combinerarecategories<-function(data_frame,mincount){ 
-  for (i in 1 : ncol(data_frame)){
+#------------------------------------------------------------------------------#
+#---------------------------CHECK FOR RARE VALUES------------------------------#
+#------------------------------------------------------------------------------#
+combinerarecategories <- function(data_frame,mincount){ 
+  for (i in 2 : ncol(data_frame)){
     a<-data_frame[,i]
     replace <- names(which(table(a) < mincount))
     levels(a)[levels(a) %in% replace] <-paste("Other",colnames(data_frame)[i],sep=".")
     data_frame[,i]<-a }
-  return(data_frame) }
+  return(data_frame)
+}
+
+cd_df <- combinerarecategories(cd_df,20) #combine categories with <20 values in STCdata into "Other"
+
+str(cd_df)
+
+#------------------------------------------------------------------------------#
+#-----------------------BASIC EXPLORATORY ANALYSIS-----------------------------#
+#------------------------------------------------------------------------------#
+
+#Correlation matrix
+GGally::ggcorr(cd_df[,-9], hjust = 1, layout.exp = 2, label = T, label_size = 2.9)
 
 
-#Apply combinerarecategories function to the data and then split it into testing and training data.
+#------------------------------------------------------------------------------#
+#---------------------------FEATURE ENGINEERING--------------------------------#
+#------------------------------------------------------------------------------#
 
-Credit_Data<-combinerarecategories(Credit_Data,20) #combine categories with <20 values in STCdata into "Other"
+### 1. Look at the delta's between periods (e.g. diff Bill_AMT2 -> Bill_AMT1)
+
+
+
+
+#------------------------------------------------------------------------------#
+#---------------------------CREATE TRAIN AND TEST SET--------------------------#
+#------------------------------------------------------------------------------#
 
 set.seed(77850) #set a random number generation seed to ensure that the split is the same everytime
-inTrain <- createDataPartition(y = Credit_Data$Retained.in.2012.,
-                               p = 1888/2389, list = FALSE)
-training <- Credit_Data[ inTrain,]
-testing <- Credit_Data[ -inTrain,]
+cd_df_inTrain <- createDataPartition(y = cd_df$default_0,
+                                     p = 0.8, list = FALSE)
 
-# Select the variables to be included in the "base-case" model
-# First include all variables use glm(Retained.in.2012.~ ., data=training, family="binomial"(link="logit")) Then see which ones have "NA" in coefficients and remove those
+cd_df_training <- cd_df[ cd_df_inTrain,]
+cd_df_testing <- cd_df[ -cd_df_inTrain,]
 
-model_logistic<-glm(Retained.in.2012.~ Special.Pay + 
-                      To.Grade + Group.State + Is.Non.Annual. +
-                      Tuition + FRP.Active + FRP.Cancelled + FRP.Take.up.percent. + 
-                      Cancelled.Pax + Total.Discount.Pax + Initial.System.Date + 
-                      Poverty.Code + CRM.Segment + School.Type + Parent.Meeting.Flag + 
-                      MDR.Low.Grade + MDR.High.Grade + Total.School.Enrollment + 
-                      EZ.Pay.Take.Up.Rate + School.Sponsor +  
-                      SPR.New.Existing + FPP + FirstMeeting + LastMeeting + 
-                      DifferenceTraveltoFirstMeeting + DepartureMonth  + MajorProgramCode + SingleGradeTripFlag + 
-                      FPP.to.School.enrollment + FPP.to.PAX + SchoolSizeIndicator, data=training, family="binomial"(link="logit"))
+colnames(cd_df_training)
+colnames(cd_df_testing)
+nrow(cd_df_training)
+nrow(cd_df_testing)
 
-summary(model_logistic) 
+#------------------------------------------------------------------------------#
+#--------------------MODEL: LOGISTIC REGRESSION--------------------------------#
+#------------------------------------------------------------------------------#
 
-# to add surrogates paste this to the list of variables; note, it will run quite a bit slower
-#Special.Pay_surrogate + Early.RPL_surrogate + Latest.RPL_surrogate + 
-#Initial.System.Date_surrogate + CRM.Segment_surrogate + MDR.High.Grade_surrogate + 
-#Total.School.Enrollment_surrogate + FirstMeeting_surrogate + 
-#LastMeeting_surrogate + DifferenceTraveltoFirstMeeting_surrogate + 
-#DifferenceTraveltoLastMeeting_surrogate + FPP.to.School.enrollment_surrogate
+#Step 1: Set-up the logistic model
+cd_df_model_logistic<-glm(default_0 ~ 
+                            LIMIT_BAL + 
+                            SEX + 
+                            EDUCATION + 
+                            MARRIAGE +
+                            AGE + 
+                            PAY_1 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + 
+                            BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + 
+                            PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6,
+                          data=cd_df_training, family="binomial"(link="logit"))
 
-##The model clearly has too many variables, most of which are insignificant 
+summary(cd_df_model_logistic) 
 
-## Stepwise regressions. There are three aproaches to runinng stepwise regressions: backward, forward and "both"
-## In either approach we need to specify criterion for inclusion/exclusion. Most common ones: based on information criterion (e.g., AIC) or based on significance  
-model_logistic_stepwiseAIC<-stepAIC(model_logistic,direction = c("both"),trace = 1) #AIC stepwise
-summary(model_logistic_stepwiseAIC) 
+#------------------------------------------------------------------------------
+#Step 2: STEPWISE AIC -> drop the insignificant variables to improve the model
+cd_df_model_logistic_stepwiseAIC<-stepAIC(cd_df_model_logistic,direction = c("both"),trace = 1) #AIC stepwise
+summary(cd_df_model_logistic_stepwiseAIC) 
+#------------------------------------------------------------------------------
 
 par(mfrow=c(1,4))
-plot(model_logistic_stepwiseAIC) #Error plots: similar nature to lm plots
+plot(cd_df_model_logistic_stepwiseAIC) #Error plots: similar nature to lm plots
 par(mfrow=c(1,1))
 
-###Finding predicitons: probabilities and classification
-logistic_probabilities<-predict(model_logistic_stepwiseAIC,newdata=testing,type="response") #Predict probabilities
-logistic_classification<-rep("1",500)
-logistic_classification[logistic_probabilities<0.6073]="0" #Predict classification using 0.6073 threshold. Why 0.6073 - that's the average probability of being retained in the data. An alternative code: logistic_classification <- as.integer(logistic_probabilities > mean(testing$Retained.in.2012. == "1"))
-logistic_classification<-as.factor(logistic_classification)
+#Step 3: Determine probabilities and classification
+cd_df_model_logistic_stepwiseAIC_probabilities<-predict(cd_df_model_logistic_stepwiseAIC,
+                                                        newdata=cd_df_testing,
+                                                        type="response") #Predict probabilities
 
-###Confusion matrix  
-confusionMatrix(logistic_classification,testing$Retained.in.2012.,positive = "1") #Display confusion matrix
+cd_df_model_logistic_stepwiseAIC_probabilities
 
-####ROC Curve
-logistic_ROC_prediction <- prediction(logistic_probabilities, testing$Retained.in.2012.)
-logistic_ROC <- performance(logistic_ROC_prediction,"tpr","fpr") #Create ROC curve data
-plot(logistic_ROC) #Plot ROC curve
+#------------------------------------------------------------------------------#
+#--------------------FIND OPTIMAL T-THRESHOLD----------------------------------#
+#------------------------------------------------------------------------------#
 
-####AUC (area under curve)
-auc.tmp <- performance(logistic_ROC_prediction,"auc") #Create AUC data
-logistic_auc_testing <- as.numeric(auc.tmp@y.values) #Calculate AUC
-logistic_auc_testing #Display AUC value: 90+% - excellent, 80-90% - very good, 70-80% - good, 60-70% - so so, below 60% - not much value
+t_threshold <- 0
+t_threshold <- as.double(t_threshold)
+best_t_threshold <- 0
+max_expected_return <- 0
 
-#### Lift chart
-plotLift(logistic_probabilities, testing$Retained.in.2012., cumulative = TRUE, n.buckets = 10) # Plot Lift chart
+while (t_threshold < 0.95) {
+  t_threshold = as.double(t_threshold+0.01)
+  
+  print(paste0("Current T-threshold applied to classification: ", t_threshold))
+  
+  cd_df_logistic_classification <-rep("1",nrow(cd_df_testing)) #Generate number of rows depending on size of training set
+  cd_df_logistic_classification[cd_df_model_logistic_stepwiseAIC_probabilities<t_threshold]="0"
+  
+  print("T-threshold applied...")
+  
+  cd_df_logistic_classification<-as.factor(cd_df_logistic_classification)
+  
+  cd_df_logistic_classification
+  
+  #Need to check if the predictor and actual outcomes have the same level
+  str(cd_df_logistic_classification)
+  #levels(cd_df_testing$default_0) <- list("0" = "1", "1" = "2")
+  str(cd_df_testing$default_0)
+  print("")
+  
+  #----------------------------------------------------------------------------#
+  #--------------------RESULTS: LOGISTIC REGRESSION----------------------------#
+  #----------------------------------------------------------------------------#
+  
+  #confusion matrix
+  cm_1 <- confusionMatrix(cd_df_logistic_classification, 
+                          cd_df_testing$default_0, 
+                          positive = "0",) ###############---> According to data dictionary, positive outcome = 0
+  
+  cm_1
+  
+  True_non_defaulter_TP <- as.integer(cm_1$table[1])
+  False_defaulter_FP <- as.integer(cm_1$table[2])
+  
+  False_non_defaulter_FN <- as.integer(cm_1$table[3])
+  True_defaulter_TN <- as.integer(cm_1$table[4])
+  
+  ####ROC Curve
+  ####AUC (area under curve)
+  #### Lift chart
+  
+  #------------------------------------------------------------------------------#
+  #--------------------RESULTS: BUSINESS OUTCOME---------------------------------#
+  #------------------------------------------------------------------------------#
+  
+  
+  Default_Cost = -5000 #as provided in the case
+  No_Default_Profit = 1500#as provided in the case
+  
+  expected_return = (Default_Cost*False_non_defaulter_FN) + (No_Default_Profit*True_non_defaulter_TP)
+  
+  print(paste0("The expected return with this T-threshold is: ", expected_return))
+  
+  if (expected_return > max_expected_return) {
+    best_t_threshold <- t_threshold
+  }
+  
+  if (expected_return > max_expected_return) {
+    max_expected_return <- expected_return
+  }
+  
+  print("")
+  print("")
+}
+
+print(paste0("The maximum expected return is realized with T-threshold = ", best_t_threshold))
+print(paste0("The maximum expected return equals ", max_expected_return))
+
+##TO do: 
+#1.Create diff features, 
+#2.finalize the logistic regression results, 
+#3.Determine the Optimal T-threshold returning the highest expected value
+#4.Exploratory data analysis in Tableau to identify features to be added
+
+
+
+
+
+
+
+
+
 
