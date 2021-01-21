4908c7637599f2289410c976d9bca14c45ab4c60
KristianJB <kristian.branaes@gmail.com>
2021-01-21 11:14:47 +0100

    XG Boost model
    Added XB boost model inc model eval code

--- Assignment2.R
+++ Assignment2.R
@@ -321,6 +321,43 @@ ggplot(output_logistic_exp_value, aes(x=A, y=B)) +
 #5.Plot the payoffs for different T-thresholds
 
 
+#------------------------------------------------------------------------------#
+#----------------------------- XGBOOST MODEL ----------------------------------#
+#------------------------------------------------------------------------------#
+
+pacman::p_load("caret","ROCR","lift","xgboost") #Check, and if needed install the necessary packages
+
+#use same training and testing data to ensure like-for-like comparison
+#note - additional memory must be allocated to r before running this code
+
+memory.limit(size=6000000)
+
+training.XG <-model.matrix(default_0 ~ ., data = cd_df_training)
+testing.XG <-model.matrix(default_0 ~ ., data = cd_df_testing)
+
+model_XGboost<-xgboost(data = data.matrix(training.XG[,-1]), 
+                       label = as.numeric(as.character(cd_df_training$default_0)), 
+                       eta = 0.1,       # hyperparameter: learning rate 
+                       max_depth = 20,  # hyperparameter: size of a tree in each boosting iteration
+                       nround=50,       # hyperparameter: number of boosting iterations  
+                       objective = "binary:logistic"
+)
+
+XGboost_prediction<-predict(model_XGboost,newdata=testing.XG[,-1], type="response") #Predict classification (for confusion matrix)
+confusionMatrix(as.factor(ifelse(XGboost_prediction>0.22,1,0)),cd_df_testing$default_0,positive="1") #Display confusion matrix
+
+####ROC Curve
+XGboost_pred_testing <- prediction(XGboost_prediction, cd_df_testing$default_0) #Calculate errors
+XGboost_ROC_testing <- performance(XGboost_pred_testing,"tpr","fpr") #Create ROC curve data
+plot(XGboost_ROC_testing) #Plot ROC curve
+
+####AUC
+auc.tmp <- performance(XGboost_pred_testing,"auc") #Create AUC data
+XGboost_auc_testing <- as.numeric(auc.tmp@y.values) #Calculate AUC
+XGboost_auc_testing #Display AUC value: 90+% - excellent, 80-90% - very good, 70-80% - good, 60-70% - so so, below 60% - not much value
+
+#### Lift chart
+plotLift(XGboost_prediction, cd_df_testing$default_0, cumulative = TRUE, n.buckets = 10) # Plot Lift chart
 
 
 
