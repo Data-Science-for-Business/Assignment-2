50af8d3c22d5a1f91caea3f55fc8269536a9d64b
KevinDECROCK <77623420+KevinDECROCK@users.noreply.github.com>
2021-01-22 00:55:28 +0100

    Update Assignment2.R

--- Assignment2.R
+++ Assignment2.R
@@ -219,12 +219,12 @@ cd_df_model_logistic<-glm(default_0 ~
                             AGE + 
                             PAY_1 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + NEG_BILL +
                             BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + 
-                            PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6 +
+                            PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6,
                             #Delta_Bill_AMT1_vs_Bill_AMT2 + Delta_Bill_AMT2_vs_Bill_AMT3 + Delta_Bill_AMT3_vs_Bill_AMT4 +Delta_Bill_AMT4_vs_Bill_AMT5 + Delta_Bill_AMT5_vs_Bill_AMT6 + 
-                            PER_Delta_Bill_AMT1_vs_Bill_AMT2 + PER_Delta_Bill_AMT2_vs_Bill_AMT3 + PER_Delta_Bill_AMT3_vs_Bill_AMT4 + PER_Delta_Bill_AMT4_vs_Bill_AMT5 + PER_Delta_Bill_AMT5_vs_Bill_AMT6 +
+                            #PER_Delta_Bill_AMT1_vs_Bill_AMT2 + PER_Delta_Bill_AMT2_vs_Bill_AMT3 + PER_Delta_Bill_AMT3_vs_Bill_AMT4 + PER_Delta_Bill_AMT4_vs_Bill_AMT5 + PER_Delta_Bill_AMT5_vs_Bill_AMT6 +
                             #Balance_remaining_1 + Balance_remaining_2 + Balance_remaining_3 + Balance_remaining_4 +Balance_remaining_5 + Balance_remaining_6 +
-                            Limit_Alert_1 + Limit_Alert_2 + Limit_Alert_3 + Limit_Alert_4 + Limit_Alert_5 +Limit_Alert_6 +
-                            mean_pay_category + Max_Pay_Category + Range_Pay_Category + abs_range_bill_amt + PER_abs_range_bill_amt_vs_Lim_balance + Max_Delta_Pay_vs_Bill,
+                            #Limit_Alert_1 + Limit_Alert_2 + Limit_Alert_3 + Limit_Alert_4 + Limit_Alert_5 +Limit_Alert_6 +
+                            #mean_pay_category + Max_Pay_Category + Range_Pay_Category + abs_range_bill_amt + PER_abs_range_bill_amt_vs_Lim_balance + Max_Delta_Pay_vs_Bill,
                           data=cd_df_training, family="binomial"(link="logit"))
 
 summary(cd_df_model_logistic) 
@@ -269,8 +269,8 @@ while (t_threshold < 0.50) {
   print(paste0("Current T-threshold applied to classification: ", t_threshold))
   
   cd_df_logistic_classification <-rep("1",nrow(cd_df_testing)) #Generate number of rows depending on size of training set
+  #cd_df_logistic_classification[cd_df_model_logistic_stepwiseAIC_probabilities<t_threshold]="0"
   cd_df_logistic_classification[cd_df_model_logistic_stepwiseAIC_probabilities<t_threshold]="0"
-  
   print("T-threshold applied...")
   
   cd_df_logistic_classification<-as.factor(cd_df_logistic_classification)
@@ -333,9 +333,7 @@ print(paste0("The maximum expected return equals ", max_expected_return))
 ###Final output logistic regression
 my_t_threshold_list
 my_expected_value_list
-cm_1 <- confusionMatrix(cd_df_logistic_classification,
-                        cd_df_testing$default_0, 
-                        positive = "0")
+cm_1
 
 output_logistic_exp_value <- do.call(rbind, Map(data.frame, A=my_t_threshold_list, B=my_expected_value_list))
 
@@ -355,6 +353,10 @@ ggplot(output_logistic_exp_value, aes(x=A, y=B)) +
 #3.Determine the Optimal T-threshold returning the highest expected value
 #4.Exploratory data analysis in Tableau to identify features to be added
 #5.Plot the payoffs for different T-thresholds
+
+
+
+
 
 
 #------------------------------------------------------------------------------#
@@ -499,4 +501,59 @@ view(logistic_classification)
 
 write.csv(logistic_classification, file = "ToSelectApplicant.csv")
 write.csv(logistic_classification, file = "ThreatToDemocracyPrediction.csv")
-  
+
+
+#----------------------------------------------------------------------------#
+#--------------------XG BOOST MODEL -----------------------------------------#
+#----------------------------------------------------------------------------#
+
+pacman::p_load("caret","ROCR","lift","xgboost") #Check, and if needed install the necessary packages
+
+#use same training and testing data to ensure like-for-like comparison
+#note - additional memory must be allocated to r before running this code
+
+memory.limit(size=6000000)
+
+training.XG <-model.matrix(default_0 ~ 
+                             LIMIT_BAL + 
+                             SEX + 
+                             EDUCATION + 
+                             MARRIAGE +
+                             AGE + 
+                             PAY_1 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + NEG_BILL +
+                             BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + 
+                             PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6, data = cd_df_training)
+testing.XG <-model.matrix(default_0 ~ 
+                            LIMIT_BAL + 
+                            SEX + 
+                            EDUCATION + 
+                            MARRIAGE +
+                            AGE + 
+                            PAY_1 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6 + NEG_BILL +
+                            BILL_AMT1 + BILL_AMT2 + BILL_AMT3 + BILL_AMT4 + BILL_AMT5 + BILL_AMT6 + 
+                            PAY_AMT1 + PAY_AMT2 + PAY_AMT3 + PAY_AMT4 + PAY_AMT5 + PAY_AMT6, data = cd_df_testing)
+
+model_XGboost<-xgboost(data = data.matrix(training.XG[,-1]), 
+                       label = as.numeric(as.character(cd_df_training$default_0)), 
+                       eta = 0.1,       # hyperparameter: learning rate 
+                       max_depth = 20,  # hyperparameter: size of a tree in each boosting iteration
+                       nround=50,       # hyperparameter: number of boosting iterations  
+                       objective = "binary:logistic"
+)
+
+XGboost_prediction<-predict(model_XGboost,newdata=testing.XG[,-1], type="response") #Predict classification (for confusion matrix)
+confusionMatrix(as.factor(ifelse(XGboost_prediction>0.22,1,0)),cd_df_testing$default_0,positive="1") #Display confusion matrix
+
+####ROC Curve
+XGboost_pred_testing <- prediction(XGboost_prediction, cd_df_testing$default_0) #Calculate errors
+XGboost_ROC_testing <- performance(XGboost_pred_testing,"tpr","fpr") #Create ROC curve data
+plot(XGboost_ROC_testing) #Plot ROC curve
+
+####AUC
+auc.tmp <- performance(XGboost_pred_testing,"auc") #Create AUC data
+XGboost_auc_testing <- as.numeric(auc.tmp@y.values) #Calculate AUC
+XGboost_auc_testing #Display AUC value: 90+% - excellent, 80-90% - very good, 70-80% - good, 60-70% - so so, below 60% - not much value
+
+#### Lift chart
+plotLift(XGboost_prediction, cd_df_testing$default_0, cumulative = TRUE, n.buckets = 10) # Plot Lift chart
+
